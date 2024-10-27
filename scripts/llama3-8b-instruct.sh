baf84f7a2aa7b0e6f40d336f6c6565091ce5b508
Dune-Z <dune.ustc@gmail.com>
2024-10-27 11:48:00 -0500

    revise shell scripts

--- scripts/llama3-8b-instruct.sh
+++ scripts/llama3-8b-instruct.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-OUTPUT_DIR="outputs/llama3-8b-instruct-restem"
+set -e
+
+OUTPUT_DIR="outputs/llama-3-8b-instruct-restem"
 ROUND=1
 NUM_ROUNDS=5
 
-for ((i=1; i<=$NUM_ROUNDS; i++)); do
-    MODEL_NAME_OR_PATH="${OUTPUT_DIR}/round-${i}/" if [ $i -eq 1 ]; then MODEL_NAME_OR_PATH="unsloth/llama3-8b-instruct"; fi
+for ((i=1; i<=$NUM_ROUNDS; i++))
+do
+    if [ $i -eq 1 ]; then
+        MODEL_NAME_OR_PATH="unsloth/llama-3-8b-instruct"
+    else
+        MODEL_NAME_OR_PATH="${OUTPUT_DIR}/round-$((i-1))/"
+    fi
     ROUND_DIR="${OUTPUT_DIR}/round-${ROUND}"
+    mkdir -p $ROUND_DIR
+
     CUDA_VISIBLE_DEVICES=0 python src/sample.py\
-        --output_file "${ROUND_DIR}/sample_output.jsonl"\
-        --model_name_or_path $MODEL_NAME_OR_PATH
+        output_file="${ROUND_DIR}/sample_output.jsonl"\
+        model_name_or_path=$MODEL_NAME_OR_PATH
     
     CUDA_VISIBLE_DEVICES=0 python src/train.py\
-        --dataset_path "${ROUND_DIR}/sample_output.jsonl"\
-        --output_dir "${ROUND_DIR}/round-${i}"
+        dataset_path="${ROUND_DIR}/sample_output.jsonl"\
+        output_dir="${ROUND_DIR}/round-${i}"
 done
