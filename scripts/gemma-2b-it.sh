e0366e09c9ccac4626955d5f77f0a2b1056b769e
Dune-Z <dune.ustc@gmail.com>
2024-10-28 10:21:31 -0500

    change problem size

--- scripts/gemma-2b-it.sh
+++ scripts/gemma-2b-it.sh
@@ -11,7 +11,6 @@ for ((i=1; i<=$NUM_ROUNDS; i++))
 do
     ROUND_DIR="${OUTPUT_DIR}/round-${ROUND}"
     SAMPLE_FILE="${ROUND_DIR}/sample_output.jsonl"
-    ACC_FILE="${ROUND_DIR}/accuracy.txt"
     BEST_MODEL_PATH="${ROUND_DIR}/best_model"
     mkdir -p $ROUND_DIR
 
@@ -21,17 +20,19 @@ do
             output_file="$SAMPLE_FILE"\
             model_name_or_path=$CURRENT_SAMPLE_MODEL_PATH\
             sample_batch_size=16\
-            dataset_kwargs.MATH.problem_size=32
+            dataset_kwargs.MATH.problem_size=-1
     else
-        echo "Sample file already exists for round $i. Skipping sampling."
+        echo "Sample file already exists for round $i. Skip sampling."
     fi
 
     BEST_ACC=0.0
+    INNER_ROUND=1
     CURRENT_MODEL_PATH=$BASE_MODEL_PATH
     TEMP_MODEL_PATH=$CURRENT_MODEL_PATH
 
     while true; do
         echo "Evaluating model for round $i"
+        ACC_FILE="${ROUND_DIR}/accuracy-${INNER_ROUND}.txt"
         CUDA_VISIBLE_DEVICES=0 python src/evaluate.py\
             model_name_or_path=$CURRENT_MODEL_PATH\
             output_file="$ACC_FILE"
@@ -40,7 +41,7 @@ do
         echo "Curr accuracy: $CURRENT_ACC"
         echo "Best accuracy: $BEST_ACC"
 
-        if (( $(echo "$CURRENT_ACC > $BEST_ACC" | bc -l) )); then
+        if (( $(echo "$CURRENT_ACC >= $BEST_ACC" | bc -l) )); then
             echo "Accuracy improved from $BEST_ACC to $CURRENT_ACC. Train new model."
             BEST_ACC=$CURRENT_ACC
 
@@ -50,12 +51,13 @@ do
                 trainer.output_dir="$ROUND_DIR"
 
             TEMP_MODEL_PATH=$CURRENT_MODEL_PATH
-            CURRENT_MODEL_PATH=$ROUND_DIR/checkpoint-*
+            mv $ROUND_DIR/checkpoint-* "${ROUND_DIR}/checkpoint-inner-round-${INNER_ROUND}"
+            CURRENT_MODEL_PATH="${ROUND_DIR}/checkpoint-inner-round-${INNER_ROUND}"
 
             python $CURRENT_MODEL_PATH/zero_to_fp32.py\
-                --safe_serialization \
                 $CURRENT_MODEL_PATH $CURRENT_MODEL_PATH
 
+            INNER_ROUND=$((INNER_ROUND+1))
         else
             echo "Accuracy did not improve. Stop training."
             mv $TEMP_MODEL_PATH "$BEST_MODEL_PATH"
