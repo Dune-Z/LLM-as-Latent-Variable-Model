0d0964b2da9da2e873ac9e541612b03958546df8
Dune-Z <dune.ustc@gmail.com>
2024-11-23 21:45:00 -0600

    revise world size

--- scripts/sft-gemma-2-2b-it/run_sample.sh
+++ scripts/sft-gemma-2-2b-it/run_sample.sh
@@ -1,11 +1,10 @@
-MODEL_PATH=google/gemma-2-2b-it
+MODEL_NAME=google/gemma-2-2b-it
 OUTPUT_PATH=outputs/sft_gemma-2-2b-it
 CUDA_VISIBLE_DEVICES=0,1,2,3
 
-# torchrun --nproc_per_node=4 sft_src/sample.py\
 python sft_src/sample.py\
     output_file="$OUTPUT_PATH/sample_output.jsonl"\
-    model_name_or_path=$MODEL_PATH\
+    model_name_or_path=$MODEL_NAME\
     sample_batch_size=128\
     dataset_kwargs.METAMATH.problem_size=512\
     dataset_kwargs.METAMATH.sample_size=1\
