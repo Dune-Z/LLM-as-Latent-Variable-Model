9d4b49e228a16fc16fdeba4964bd14287952b967
Dune-Z <dune.ustc@gmail.com>
2024-11-24 21:55:30 -0600

    disable wandb

--- sft_src/baseline_sft.py
+++ sft_src/baseline_sft.py
@@ -157,7 +157,7 @@ training_args = TrainingArguments(
     logging_steps=1,
     push_to_hub=script_args.push_to_hub,
     hub_model_id=script_args.hub_model_id,
-    report_to='no'
+    report_to="none"
 )
 
 model_kwargs = dict(
